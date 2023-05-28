ab72c24e4e0d12be2e19a56a115ba51768eeb9c6
kolosovpetro <kolosovp94@gmail.com>
2023-05-28 12:12:38 +0200

    work in progress

--- kubernetes/create-aks-cluster.ps1
+++ kubernetes/create-aks-cluster.ps1
@@ -7,6 +7,8 @@ az group create --location "northeurope" --subscription "f32f6566-8fa0-4198-9c91
 # create AKS cluster
 az aks create --generate-ssh-keys --subscription "f32f6566-8fa0-4198-9c91-a3b8ac69e89a" --node-count 3 --resource-group "aks-k8s-rg" --name "aks-k8s" --tier free
 
+az aks update -n "aks-k8s" -g "aks-k8s-rg" --attach-acr "acrcarsislandd01"
+
 # connect to cluster
 az aks get-credentials --resource-group "aks-k8s-rg" --name "aks-k8s" --subscription "f32f6566-8fa0-4198-9c91-a3b8ac69e89a"
 
