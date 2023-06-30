b57886acaa85c91f57d56ee64407320caa75f285
kolosovpetro <kolosovp94@gmail.com>
2023-07-01 01:44:41 +0200

    work in progress

--- kubernetes/create-aks-cluster.ps1
+++ kubernetes/create-aks-cluster.ps1
@@ -6,7 +6,7 @@ az group create --location "northeurope" --subscription "f32f6566-8fa0-4198-9c91
 
 # create AKS cluster
 az aks create --generate-ssh-keys --subscription "f32f6566-8fa0-4198-9c91-a3b8ac69e89a" --node-count 3 --resource-group "aks-k8s-rg" --name "aks-k8s" --tier free
-az aks create --generate-ssh-keys --subscription "f32f6566-8fa0-4198-9c91-a3b8ac69e89a" --node-count 3 --resource-group "cars-island-d01" --name "cars-aks-k8s-d01" --tier free
+az aks create --generate-ssh-keys --subscription "f32f6566-8fa0-4198-9c91-a3b8ac69e89a" --node-count 3 --resource-group "cars-island-c01" --name "cars-aks-k8s-c01" --attach-acr "acrcarsislandc01" --tier free
 
 az aks update -n "aks-k8s" -g "aks-k8s-rg" --attach-acr "acrcarsislandd01"
 az aks update -n "cars-aks-k8s-d01" -g "cars-island-d01" --attach-acr "acrcarsislandd01"
@@ -14,6 +14,7 @@ az aks update -n "cars-aks-k8s-d01" -g "cars-island-d01" --attach-acr "acrcarsis
 # connect to cluster
 az aks get-credentials --resource-group "aks-k8s-rg" --name "aks-k8s" --subscription "f32f6566-8fa0-4198-9c91-a3b8ac69e89a"
 az aks get-credentials --resource-group "cars-island-d01" --name "cars-aks-k8s-d01" --subscription "f32f6566-8fa0-4198-9c91-a3b8ac69e89a"
+az aks get-credentials --resource-group "cars-island-c01" --name "cars-aks-k8s-c01" --subscription "f32f6566-8fa0-4198-9c91-a3b8ac69e89a"
 
 # get access to Dashboard
 kubectl create clusterrolebinding kubernetes-dashboard --clusterrole=cluster-admin --serviceaccount=kube-system:kubernetes-dashboard
@@ -21,6 +22,7 @@ kubectl create clusterrolebinding kubernetes-dashboard --clusterrole=cluster-adm
 # Open Dashboard
 az aks browse --resource-group "aks-k8s-rg" --name "aks-k8s" --subscription "f32f6566-8fa0-4198-9c91-a3b8ac69e89a"
 az aks browse --resource-group "cars-island-d01" --name "cars-aks-k8s-d01" --subscription "f32f6566-8fa0-4198-9c91-a3b8ac69e89a"
+az aks browse --resource-group "cars-island-c01" --name "cars-aks-k8s-c01" --subscription "f32f6566-8fa0-4198-9c91-a3b8ac69e89a"
 
 # Create config map
 kubectl create configmap "cars-configmap" --from-file=cars-configmap.yaml
